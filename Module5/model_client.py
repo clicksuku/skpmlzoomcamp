5acf0420537979a38e1105d6383de945a2addf6f
Sundar KP <1720269+clicksuku@users.noreply.github.com>
2025-10-22 08:51:53 +0530

    Module 5  of ML Zoomcamp

--- Module5/model_client.py
+++ Module5/model_client.py
@@ -3,9 +3,9 @@ import requests
 url = "http://127.0.0.1:8000/predict"
 
 lead = {
-    "lead_source": "paid_ads",
-    "number_of_courses_viewed": 2,
-    "annual_income": 79276.0
+    "lead_source": "organic_search",
+    "number_of_courses_viewed": 4,
+    "annual_income": 80304.0
 }
 
 response = requests.post(url, json=lead)
